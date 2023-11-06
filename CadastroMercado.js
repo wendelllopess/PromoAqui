933ec44fe79c976eb0412619e7a9a39b45bd75b8
wendelllopess <wendelllopes020@gmail.com>
2023-11-06 17:58:47 -0300

    comecand o back end

---
+++ CadastroMercado.js
@@ -0,0 +1,79 @@
+const formulario_cadastro = document.getElementById("formulario_cadastro");
+
+const nome_fantasia = document.getElementById("nome_fantasia");
+const razao_social = document.getElementById("razao_social");
+const cnpj = document.getElementById("cnpj");
+const telefone_empresa = document.getElementById("telefone_empresa");
+const website_empresa = document.getElementById("website_empresa");
+const cep = document.getElementById("cep");
+const uf = document.getElementById("uf");
+const cidade = document.getElementById("cidade");
+const bairro = document.getElementById("bairro");
+const numero_casa = document.getElementById("numero_casa");
+const complemento = document.getElementById("complemento");
+const nome_completo = document.getElementById("nome_completo");
+const funcao = document.getElementById("funcao");
+const email = document.getElementById("email");
+const telefone_responsavel = document.getElementById("telefone_responsavel");
+const senha = document.getElementById("senha");
+
+function cadastrar() {
+  fetch("http://localhost:8080/CadastroMercado", {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    method: "post",
+    body: JSON.stringify({
+      nome_fantasia: nome_fantasia.value,
+      razao_social: razao_social.value,
+      cnpj: cnpj.value,
+      telefone_empresa: telefone_empresa.value,
+      website_empresa: website_empresa.value,
+      cep: cep,
+      uf: uf.value,
+      cidade: cidade.value,
+      bairro: bairro.value,
+      numero_casa: numero_casa.value,
+      complemento: complemento.value,
+      nome_completo: nome_completo.value,
+      funcao: funcao.value,
+      email: email.value,
+      telefone_responsavel: telefone_responsavel.value,
+      senha: senha.value,
+    }),
+  })
+    .then(function (res) {
+      console.log(res);
+    })
+    .catch(function (res) {
+      console.log(res);
+    });
+}
+
+function limpar_campos_formulario() {
+  nome_fantasia.value = "";
+  razao_social.value = "";
+  cnpj.value = "";
+  telefone_empresa.value = "";
+  website_empresa.value = "";
+  cep.value = "";
+  uf.value = "";
+  cidade.value = "";
+  bairro.value = "";
+  numero_casa.value = "";
+  complemento.value = "";
+  nome_completo.value = "";
+  funcao.value = "";
+  email.value = "";
+  telefone_responsavel.value = "";
+  senha.value = "";
+}
+
+formulario_cadastro.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  cadastrar();
+
+  // limpar_campos_formulario();
+});
--- login.js
+++ login.js
@@ -1,29 +1,29 @@
-const formulario = document.getElementById("formulario")
+// const formulario = document.getElementById("formulario")
 
-formulario.addEventListener("submit", (e) =>{
-    e.preventDefault();
+// formulario.addEventListener("submit", (e) =>{
+//     e.preventDefault();
 
-    login();
-    }
+//     login();
+//     }
 
-)
+// )
     
 
 
-function login(){
-    const cnpj= document.getElementById("cnpj");
-    const senha = document.getElementById("senha");
+// function login(){
+//     const cnpj= document.getElementById("cnpj");
+//     const senha = document.getElementById("senha");
     
-    const cnpjDigitado = cnpj.value;
-    const senhaDigitada = senha.value;
+//     const cnpjDigitado = cnpj.value;
+//     const senhaDigitada = senha.value;
 
-    if(cnpjDigitado === "teste" && senhaDigitada === "123"){
-        window.location.href="poslogin.html"
-    }else{
-        alert("algo esta errado ")
-    }
+//     if(cnpjDigitado === "teste" && senhaDigitada === "123"){
+//         window.location.href="poslogin.html"
+//     }else{
+//         alert("algo esta errado ")
+//     }
 
-}
+// }
 
-const botao_entrar = document.getElementById("botao_entrar")
-    botao_entrar.addEventListener("click", login)
+// const botao_entrar = document.getElementById("botao_entrar")
+//     botao_entrar.addEventListener("click", login)
